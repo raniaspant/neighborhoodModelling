c784212d9ff9ba5d9cbdc2eec212202bdde6593c
raniaspant <rania.spantidi@gmail.com>
2018-03-02 13:28:07 -0600

    Scripts

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerScript : MonoBehaviour {
+
+    NavMeshAgent agent;
+    public GameObject shelter;
+
+    public bool shouldFlee;
+
+    // Use this for initialization
+    void Start () {
+
+        shouldFlee = false;
+        agent = GetComponent<NavMeshAgent>();
+        shelter = GameObject.FindGameObjectWithTag("Shelter");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (shouldFlee)
+        {
+            
+            agent.SetDestination(shelter.transform.position);
+            Debug.Log(agent.destination + " AND " + shelter.transform.position);
+        }
+
+    }
+}
---
+++ Assets/Scripts/ShelterScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+public class ShelterScript : MonoBehaviour {
+
+    public Text numberOfPeople;
+    public int peopleInsideShelter = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Person")
+        {
+            peopleInsideShelter += 1;
+            numberOfPeople.text = peopleInsideShelter.ToString();
+        }
+    }
+}
